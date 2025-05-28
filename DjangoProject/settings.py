82fd37dab6e477210d2b0be6079c3c51466abe54
Rizwan Ahmed Rakib <rizwan35-2579@diu.edu.bd>
2025-05-29 01:29:26 +0600

    testing for cornjobs

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -148,6 +148,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
